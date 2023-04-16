af74c7724aa324e6e1114c266ff29b1e36a05ce0
Akshita Agarwal <akshita.agarwal@airbnb.com>
2023-04-16 10:12:57 +0530

    Add destination rule with maxConnectionDuration

--- setup.sh
+++ setup.sh
@@ -11,5 +11,12 @@ minikube image load pyclient:latest
 kubectl apply -f infra/pyserver.yml
 kubectl apply -f infra/pyclient.yml
 
-## Delete
-kubectl logs -l "app=pyclient" 
+#kubectl logs -l "app=pyclient" 
+
+#kubectl exec $(kubectl get pod --selector app=pyclient --output jsonpath='{.items[0].metadata.name}') -c istio-proxy -- curl -X POST http://localhost:15000/clusters | grep pyserver | grep upstream
+
+#kubectl exec -it pyserver-v1-74cf97d7d4-dtc2d -c istio-proxy -n default  -- pilot-agent request GET server_info --log_as_json | jq {version}
+
+#istioctl proxy-config cluster pyserver-v1-74cf97d7d4-dtc2d --fqdn pyserver.default.svc.cluster.local -o json
+
+#istioctl proxy-config cluster pyclient-5fdf4b5cb5-rktc7 --fqdn pyclient.default.svc.cluster.local -o json
